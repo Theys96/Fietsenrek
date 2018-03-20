d90aec8da002a07e9e811afbad122c047d2a8b12
Theys96 <accounts@thijshavinga.nl>
2018-03-20 14:25:20 +0100

    Added front end for API

--- nodejs/static/fietsenrek.js
+++ nodejs/static/fietsenrek.js
@@ -1,4 +1,25 @@
+var listElement;
+
 $(function() {
-	var info = $('#info').html("Wop");
-	info.html("Wop");
-})
+	listElement = $('#list');
+	update();
+});
+
+function update() {
+	$.getJSON('/list', function (racks) {
+		listElement.html("");
+		for (i = 0; i < racks.length; i++) {
+			div = $('<div class="rack">');
+			div.append("<h3>" + racks[i].name + "</h3>");
+			div.append("<p>size: " + racks[i].size + ", last heartbeat: " + racks[i].last_heartbeat + " ms ago</p>");
+			list = $('<div class="row">');
+			for (j = 0; j < racks[i].spots.length; j++) {
+				list.append('<div style="background-color: ' + (racks[i].spots[j] ? "salmon" : "lightgreen") + '" class="col-1 spot">');
+			}
+			div.append(list);
+			listElement.append(div);
+		}
+	});
+}
+
+// setInterval(update, 500);
